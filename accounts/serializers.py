cedf556c58458ae2eb9cd831654c401157d9fd9d
Amine <amine.hamed@esprit.tn>
2024-10-05 17:04:58 +0100

    added chatbot

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -16,7 +16,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data['email'], 
             password=validated_data['password']
         )
         
