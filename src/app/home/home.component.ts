4c68fc7c67ceb9c8950658394d6d7b495528ae70
Amine <amine.hamed@esprit.tn>
2024-10-29 18:21:27 +0100

    added home page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: [
+    './home.component.css',
+    '../../../public/lib/owlcarousel/assets/owl.carousel.min.css',
+    '../../../public/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css'
+  ]
+})
+export class HomeComponent {
+
+}
